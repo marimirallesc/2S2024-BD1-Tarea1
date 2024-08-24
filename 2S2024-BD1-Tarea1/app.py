0c71257b23a25cd32a5c4c712fd96cc0d919d85e
RandyBaeza <56372910+RandyBaeza@users.noreply.github.com>
2024-08-24 01:02:40 -0600

    Eliminacion de archivos duplicados

--- 2S2024-BD1-Tarea1/app.py
+++ 2S2024-BD1-Tarea1/app.py
@@ -40,4 +40,5 @@ def insertar_empleado():
         return jsonify({'success': False, 'message': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0')
